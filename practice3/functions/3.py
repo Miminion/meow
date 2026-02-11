0dd402be4cbe2e52950d952d65bfa27e63833ec7
«Medina» <iluvmedina@gmail.com>
2026-02-11 13:51:03 +0500

    using pass statement

--- practice3/functions/3.py
+++ practice3/functions/3.py
@@ -1,10 +1,3 @@
-def my_function(*numbers):
-  if len(numbers) == 0:
-    return None
-  max_num = numbers[0]
-  for num in numbers:
-    if num > max_num:
-      max_num = num
-  return max_num
-
-print(my_function(3, 7, 2, 9, 1))
+def my_function():
+  pass
+#The pass statement is often used when developing, allowing you to define the structure first and implement details later.
