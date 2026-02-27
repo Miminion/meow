6920a9c4a3c8341f9b6ae03533300248471beec5
«Medina» <iluvmedina@gmail.com>
2026-02-27 12:50:45 +0500

    area of polygon

--- practice4/math/2.py
+++ practice4/math/2.py
@@ -1,6 +1,6 @@
-import math
-h=int(input())
-v1=int(input())
-v2=int(input())
-tr=h/2*(v1+v2)
-print(tr)
+base   = float(input("Length of base: "))
+height = float(input("Height of parallelogram: "))
+
+area = base * height
+
+print("Expected Output:", area)
