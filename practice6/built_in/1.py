0243964719e152a5f23c576ce4c95839e5f0e78e
«Medina» <iluvmedina@gmail.com>
2026-03-20 13:02:10 +0500

    map-reduce-filter

--- practice6/built_in/1.py
+++ practice6/built_in/1.py
@@ -1,3 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-squared = list(map(lambda x: x**2, numbers))
-print(squared)
+from functools import reduce
+n= [1, 2, 3, 4, 5]
+s  = list(map(lambda x: x**2, n))
+print(s)
+e = list(filter(lambda x: x % 2 == 0, n))
+print(e)
+
+
+
+p = reduce(lambda x, y: x * y, n)
+print(p)
