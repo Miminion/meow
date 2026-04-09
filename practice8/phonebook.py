67cef044af8363ef4a29248c6a1aefbc0c9c6030
«Medina» <iluvmedina@gmail.com>
2026-04-09 14:48:01 +0500

    sql functions and phonebook ex

--- practice8/phonebook.py
+++ practice8/phonebook.py
@@ -16,6 +16,16 @@ def add_or_update(name, phone):
     conn.commit()
     conn.close()
 
+def add_many(contacts: list):
+    # contacts = [("Alice", "1234567"), ("Bob", "abc"), ...]
+    names  = [c[0] for c in contacts]
+    phones = [c[1] for c in contacts]
+    conn = connect()
+    cur  = conn.cursor()
+    cur.execute("CALL insert_many_contacts(%s::varchar[], %s::varchar[])", (names, phones))
+    conn.commit()
+    conn.close()
+
 def delete_contact(value):
     conn = connect()
     cur = conn.cursor()
@@ -35,9 +45,10 @@ def paginate(limit, offset):
 
 while True:
     print("\n1 - Search")
-    print("2 - Add or Update")
+    print("2 - Add or Update (one)")
     print("3 - Delete")
     print("4 - Pagination")
+    print("5 - Add many")
     print("0 - Exit")
 
     choice = input("Choose: ")
@@ -59,6 +70,15 @@ while True:
         l = int(input("Limit: "))
         o = int(input("Offset: "))
         paginate(l, o)
+
+    elif choice == "5":
+        contacts = []
+        count = int(input("How many contacts? "))
+        for _ in range(count):
+            n  = input("Name: ")
+            ph = input("Phone: ")
+            contacts.append((n, ph))
+        add_many(contacts)
 
     elif choice == "0":
         break
--- practice8/procedures.sql
+++ practice8/procedures.sql
@@ -18,3 +18,44 @@ BEGIN
     WHERE first_name = p_value OR phone = p_value;
 END;
 $$;
+
+
+
+CREATE OR REPLACE PROCEDURE insert_many_contacts(
+    p_names  VARCHAR[],
+    p_phones VARCHAR[]
+)
+LANGUAGE plpgsql AS $$
+DECLARE
+    i            INT;
+    bad_names    VARCHAR[] := '{}';
+    bad_phones   VARCHAR[] := '{}';
+BEGIN
+    FOR i IN 1 .. array_length(p_names, 1) LOOP
+
+        IF p_phones[i] !~ '^\+?[0-9\s\-]{7,15}$' THEN
+            bad_names  := array_append(bad_names,  p_names[i]);
+            bad_phones := array_append(bad_phones, p_phones[i]);
+            CONTINUE;  
+        END IF;
+
+        IF EXISTS (SELECT 1 FROM phonebook WHERE first_name = p_names[i]) THEN
+            UPDATE phonebook
+            SET phone = p_phones[i]
+            WHERE first_name = p_names[i];
+        ELSE
+            INSERT INTO phonebook(first_name, phone)
+            VALUES (p_names[i], p_phones[i]);
+        END IF;
+
+    END LOOP;
+
+
+    IF array_length(bad_names, 1) > 0 THEN
+        RAISE NOTICE 'uncorrect wtf:';
+        FOR i IN 1 .. array_length(bad_names, 1) LOOP
+            RAISE NOTICE 'name: %, number: %', bad_names[i], bad_phones[i];
+        END LOOP;
+    END IF;
+END;
+$$;
